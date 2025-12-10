75ce5f037dece7896c34cbe0fdc8cb54698d405e
Yusuf Talha <132570034+taLhaaktrk@users.noreply.github.com>
2025-12-10 15:02:04 +0300

    Add files via upload

---
+++ HomeScreen.js
@@ -0,0 +1,23 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Zamanlayıcı Ekranı</Text>
+      <Text>Buraya zamanlayıcı ve kategori seçimi gelecek.</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+});
